566b25171e63d51516c777bde82f1b821290e154
Robert C. Martin <unclebob@cleancoder.com>
2026-05-16 11:20:37 -0500

    Refactor XSP load save

--- internal/acceptance/xsp_steps.go
+++ internal/acceptance/xsp_steps.go
@@ -13,15 +13,16 @@ func createXSPInputWithMarker(w *world, example map[string]string) error {
 	if err != nil {
 		return err
 	}
-	if marker == "#1.0" {
+	switch marker {
+	case "#1.0":
 		w.xspInput = "#1.0\n"
 		return nil
-	}
-	if marker == "none" {
+	case "none":
 		w.xspInput = "mass 1 0 0 1 0.8\n"
 		return nil
+	default:
+		return fmt.Errorf("unsupported marker %q", marker)
 	}
-	return fmt.Errorf("unsupported marker %q", marker)
 }
 
 func loadXSPInput(w *world, _ map[string]string) error {
@@ -172,11 +173,7 @@ func assertXSPSaveEndsWithNewline(w *world, _ map[string]string) error {
 }
 
 func createXSPInputWithFileMass(w *world, example map[string]string) error {
-	id, err := stringValue(example, "mass_id")
-	if err != nil {
-		return err
-	}
-	value, err := stringValue(example, "file_mass_value")
+	id, value, err := stringPair(example, "mass_id", "file_mass_value")
 	if err != nil {
 		return err
 	}
@@ -215,11 +212,7 @@ func assertXSPMassFixedState(w *world, example map[string]string) error {
 }
 
 func assertSavedMassSign(w *world, example map[string]string) error {
-	id, err := stringValue(example, "mass_id")
-	if err != nil {
-		return err
-	}
-	sign, err := stringValue(example, "file_mass_sign")
+	id, sign, err := stringPair(example, "mass_id", "file_mass_sign")
 	if err != nil {
 		return err
 	}
