e9da95893a0611738dad65cef59f018fca995ce6
Robert C. Martin <unclebob@cleancoder.com>
2026-05-22 11:01:17 -0500

    Run mutate4go manifests file by file

--- cmd/gherkin-parser/main.go
+++ cmd/gherkin-parser/main.go
@@ -36,5 +36,5 @@ func writeFeatureJSON(featurePath, outputPath string) error {
 }
 
 // mutate4go-manifest-begin
-// {"version":1,"tested_at":"2026-05-18T21:16:51-05:00","module_hash":"17c9e96fb5ce29fec2889f28ab396b4b54fc1bbf2b58586aca9a6e39cbc58102","functions":[{"id":"func/main","name":"main","line":11,"end_line":13,"hash":"456fb961f0d5d132d6d1e97ed1c9a19d21495a7ede09523a5327d7ed85c0b4f4"},{"id":"func/run","name":"run","line":15,"end_line":25,"hash":"94f47f526ac0c1d36ed7ebb1800e4bb46300d2d40d8f776bca7cb5badf788644"},{"id":"func/writeFeatureJSON","name":"writeFeatureJSON","line":27,"end_line":36,"hash":"2c8e3a2405089e8f03c4676a0998a86a0c030a9785cf8ac59d81e1e1475b0899"}]}
+// {"version":1,"tested_at":"2026-05-22T10:51:41-05:00","module_hash":"17c9e96fb5ce29fec2889f28ab396b4b54fc1bbf2b58586aca9a6e39cbc58102","functions":[{"id":"func/main","name":"main","line":11,"end_line":13,"hash":"456fb961f0d5d132d6d1e97ed1c9a19d21495a7ede09523a5327d7ed85c0b4f4"},{"id":"func/run","name":"run","line":15,"end_line":25,"hash":"94f47f526ac0c1d36ed7ebb1800e4bb46300d2d40d8f776bca7cb5badf788644"},{"id":"func/writeFeatureJSON","name":"writeFeatureJSON","line":27,"end_line":36,"hash":"2c8e3a2405089e8f03c4676a0998a86a0c030a9785cf8ac59d81e1e1475b0899"}]}
 // mutate4go-manifest-end
--- cmd/springs-check/main.go
+++ cmd/springs-check/main.go
@@ -7,5 +7,5 @@ func main() {
 }
 
 // mutate4go-manifest-begin
-// {"version":1,"tested_at":"2026-05-18T21:15:24-05:00","module_hash":"439e81ca5ceb337031718a2a2abf48162b6f5c733ff11bb58b75b1e2971273ce","functions":[{"id":"func/main","name":"main","line":5,"end_line":7,"hash":"29f9d743352fc0bb8c72a3e77bc525306aee009024f4b05145191811406283ed"}]}
+// {"version":1,"tested_at":"2026-05-22T10:55:21-05:00","module_hash":"439e81ca5ceb337031718a2a2abf48162b6f5c733ff11bb58b75b1e2971273ce","functions":[{"id":"func/main","name":"main","line":5,"end_line":7,"hash":"29f9d743352fc0bb8c72a3e77bc525306aee009024f4b05145191811406283ed"}]}
 // mutate4go-manifest-end
